1fc7d8d36791494f88485c03c4d28f49d0fdabdb
Đào Anh Thành <daoanhthanh.work@gmail.com>
2021-08-01 20:41:11 +0700

    ok

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EntityScan(basePackages = { "com.example.demo.model.entity" })
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,50 @@
+
+
+create table features
+(
+    id          integer       not null
+        constraint features_pkey
+            primary key,
+    created_at  timestamp default now(),
+    updated_at  timestamp,
+    description varchar(6000),
+    method      varchar(10)   not null,
+    name        varchar(100)  not null,
+    path        varchar(5000) not null,
+    point       integer       not null,
+    status      varchar(20)   not null
+);
+
+create table groups
+(
+    id          integer      not null
+        constraint groups_pkey
+            primary key,
+    created_at  timestamp default now(),
+    updated_at  timestamp,
+    description varchar(200),
+    module_id   integer      not null,
+    name        varchar(100) not null
+);
+
+create table groups_features
+(
+    id         integer not null
+        constraint groups_features_pkey
+            primary key,
+    created_at timestamp default now(),
+    updated_at timestamp,
+    feature_id integer not null,
+    group_id   integer not null
+);
+
+create table modules
+(
+    id          integer      not null
+        constraint modules_pkey
+            primary key,
+    created_at  timestamp default now(),
+    updated_at  timestamp,
+    description varchar(200),
+    name        varchar(100) not null
+);
